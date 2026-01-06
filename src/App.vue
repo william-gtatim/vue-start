23cbecfe7cba5035cffbe48a7955517c790b563e
William Godoy <williamtatim91@gmail.com>
2026-01-06 11:32:53 -0300

    Configuração do primevue no projeto, preset e ícones

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Button from "primevue/button"
 </script>
 
 <template>
   <p class="text-red-500">Página inicial</p>
+  <Button label="Botão" icon="pi pi-user" />
   <RouterView />
 </template>
 
